a5a8149cde23731060bfda6484c57b1182e02173
Gulshan Raj <gulshan@cambridgecloudpartners.com>
2019-01-01 15:30:32 +0530

    check styling

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,13 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Home | E-Shopper</title>
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{asset('css/price-range.css')}}" rel="stylesheet">
-    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
-	<link href="{{asset('css/main.css')}}" rel="stylesheet">
-	<link href="{{asset('css/responsive.css')}}" rel="stylesheet">
+   
     <!--[if lt IE 9]>
     <script src="{{asset('js/html5shiv.js')}}"></script>
     <script src="{{asset('js/respond.min.js')}}"></script>
