cea43cc68a4977c80ad11b16a41e986c94553b58
Gulshan Raj <gulshan@cambridgecloudpartners.com>
2019-01-03 18:33:39 +0530

    remove is_array as parent if

--- app/Cart.php
+++ app/Cart.php
@@ -24,20 +24,17 @@ class Cart{
 
     public function addItem($id, $product){
         $price = (int) str_replace("$","",$product->price);
-        if(is_array($this->items)){
-            if(array_key_exists($id,$this->items)){
-                print_r('inside if');
-                $productToAdd = $this->items[$id];
-                $productToAdd['quantity']++;
-            }else{
-                print_r('inside else');
-                $productToAdd = ['quantity'=>1, 'price'=>$price, 'data'=>$product];
-            }
-    
-            $this->items[$id] = $productToAdd;
-            $this->totalQuantity++;
-            $this->totalPrice = $this->totalPrice + $price;
-            //dump($this->items);
+        if(is_array($this->items) && array_key_exists($id,$this->items)){
+            print_r('inside if');
+            $productToAdd = $this->items[$id];
+            $productToAdd['quantity']++;
+        }else{
+            print_r('inside else');
+            $productToAdd = ['quantity'=>1, 'price'=>$price, 'data'=>$product];
         }
+
+        $this->items[$id] = $productToAdd;
+        $this->totalQuantity++;
+        $this->totalPrice = $this->totalPrice + $price;
     }
 }
