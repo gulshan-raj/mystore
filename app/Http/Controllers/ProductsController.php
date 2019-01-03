affe489a6ef6113b4e1c89136e05345dd200ef1a
Gulshan Raj <gulshan@cambridgecloudpartners.com>
2019-01-03 22:16:18 +0530

    - Add to cart - indentation

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,11 +6,10 @@ use Illuminate\Support\Facades\DB;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Cart;
+use lluminate\Support\Facades\Session;
 
-class ProductsController extends Controller
-{
-     public function index()
-    {
+class ProductsController extends Controller{
+     public function index(){
         /*$products = [ 0=>["name"=>"Iphone","category"=>"Smart Phones","price"=>1000],
         1=>["name"=>"Galaxy","category"=>"Tablets","price"=>2000],
         2=>["name"=>"Sony","category"=>"Tv","price"=>3000],
@@ -20,13 +19,22 @@ class ProductsController extends Controller
         return view("allproducts",compact("products"));
     }
 
-    public function addProductToCart(Request $request, $id)
-    {
+    public function addProductToCart(Request $request, $id){
         $prevCart = $request->session()->get('cart');
         $cart = new Cart($prevCart);
         $product = Product::find($id);
         $cart->addItem($id,$product);
         $request->session()->put('cart',$cart);
         return redirect()->route('allProducts');
+    }
+
+    public function showCart(){
+        $cart = Session::get('cart');
+
+        if($cart){
+            return view('cartproducts', ['cartItems'=>$cart]);
+        }else{
+            echo "cart is empty";
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/test', function () {
     return view('test');
 });
 
-Route::get('/products', ["uses"=>"ProductsController@index","as"=>"allProducts"]);
+Route::get('products', ["uses"=>"ProductsController@index","as"=>"allProducts"]);
 
 Route::get('product/addToCart/{id}', ['uses'=>'ProductsController@addProductToCart','as'=>'AddToCartProduct']);
+
+Route::get('cart', ["uses"=>"ProductsController@showCart","as"=>"cartproducts"]);
