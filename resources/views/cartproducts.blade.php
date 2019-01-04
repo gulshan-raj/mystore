14db4c482ef564c087ab300376228ec7806d70fc
Gulshan Raj <gulshan@cambridgecloudpartners.com>
2019-01-04 20:02:31 +0530

    Home page link

--- resources/views/cartproducts.blade.php
+++ resources/views/cartproducts.blade.php
@@ -4,7 +4,7 @@
 		<div class="container">
 			<div class="breadcrumbs">
 				<ol class="breadcrumb">
-				  <li><a href="#">Home</a></li>
+				  <li><a href="/products">Home</a></li>
 				  <li class="active">Shopping Cart</li>
 				</ol>
 			</div>
